e2fa5124cbfedea0f76e3432d77d99be6fcb5554
jichunl <jichunli@uw.edu>
2018-07-30 10:33:17 -0700

    phase 2

---
+++ lab3/firecracker_asb.d
@@ -0,0 +1,13 @@
+
+firecracker_asb.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 04 25 0c 23 60 	movq   $0x6117a1ab,0x60230c
+   7:	00 ab a1 17 61 
+   c:	48 c7 04 25 08 23 60 	movq   $0x775c915e,0x602308
+  13:	00 5e 91 5c 77 
+  18:	68 20 10 40 00       	pushq  $0x401020
+  1d:	c3                   	retq   
---
+++ lab3/firecracker_asb.s
@@ -0,0 +1,4 @@
+movq $0x6117a1ab, 
+movq $0x775c915e, %rsi
+pushq $0x401020
+ret
